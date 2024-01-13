b8798909e347b60d66502e89057aa768e95d8738
ohs <gudtjr1355@gmail.com>
2024-01-13 20:55:46 +0900

    commit 1-13 2

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,16 +4,23 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
         // memberServiceImpl 객체를 생성하며 구현체를 매개변수로 넘김
 
-        // MemberService memberService = new MemberServiceImpl();
+        // 스프링 컨테이너
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+                                                            // AppConfig에있는 @Bean을 등록하고 관리해준다.
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+                                                                 // getBean(찾을 객체 이름, 타입) - 기본적으로 메서드 이름으로 등록됨
 
         Member member = new Member(1L, "memberA", Grade.VIP);
 
@@ -25,5 +32,5 @@ public class MemberApp {
         System.out.println("find한 new 멤버 : " + findMember.getName());
 
     }
-    
+
 }
